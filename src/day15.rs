4c772b547233281748b77763ff4a3556c5855d29
Phil Hord <phord@purestorage.com>
2022-12-10 13:32:35 -0800

    Initial commit
    I solved some of 2019 before, but I didn't save my code.  Now I'm
    redoing it in Rust.

---
+++ src/day15.rs
@@ -0,0 +1,46 @@
+#[allow(unused)]
+use yaah::aoc;
+#[allow(unused)]
+use crate::*;
+
+//------------------------------ TESTS
+#[test] fn test_day15_part1() { assert_eq!(solve1(_SAMPLE), 1); }
+#[test] fn test_day15_part2() { assert_eq!(solve2(_SAMPLE), 2); }
+
+//------------------------------ PARSE INPUT
+
+fn parse(input: &'static str) -> Vec<&str> {
+    input.lines()
+        .collect()
+}
+
+//------------------------------ SOLVE
+
+fn solve(input: &'static str, part: usize) -> usize {
+    parse(input);
+    part
+}
+
+fn solve1(input: &'static str) -> usize { solve(input, 1) }
+fn solve2(input: &'static str) -> usize { solve(input, 2) }
+
+//------------------------------ RUNNERS
+
+#[allow(unused)]
+// #[aoc(day15, part1)]
+fn day15_part1(input: &'static str) -> usize {
+    let ans = solve1(input);
+    // assert_eq!(ans, 0);
+    ans
+}
+
+#[allow(unused)]
+// #[aoc(day15, part2)]
+fn day15_part2(input: &'static str) -> usize {
+    let ans = solve2(input);
+    // assert_eq!(ans, 0);
+    ans
+}
+
+//------------------------------ SAMPLE DATA
+const _SAMPLE: &str = "1234";
