a434d85091b5ac98c7c5a7e700e65a71c9567ec8
Phil Hord <phord@purestorage.com>
2022-12-10 17:27:34 -0800

    Day 6: Orbital mechanics

--- src/day6.rs
+++ src/day6.rs
@@ -2,45 +2,107 @@
 use yaah::aoc;
 #[allow(unused)]
 use crate::*;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::ops::Sub;
 
 //------------------------------ TESTS
-#[test] fn test_day6_part1() { assert_eq!(solve1(_SAMPLE), 1); }
-#[test] fn test_day6_part2() { assert_eq!(solve2(_SAMPLE), 2); }
+#[test] fn test_day6_part1() { assert_eq!(solve1(_SAMPLE), 42); }
+#[test] fn test_day6_part2() { assert_eq!(solve2(_SAMPLE2), 4); }
 
 //------------------------------ PARSE INPUT
 
-fn parse(input: &'static str) -> Vec<&str> {
+fn parse(input: &'static str) -> HashMap<&str, &str> {
     input.lines()
+        .map(|x| { let (a,b) = x.split_once(')').unwrap(); (b,a)} )
         .collect()
 }
 
 //------------------------------ SOLVE
 
-fn solve(input: &'static str, part: usize) -> usize {
-    parse(input);
-    part
+fn dist_to_com(map: &HashMap<&str, &str>, node: &str) -> usize {
+    match map.get(node) {
+        Some(node) => 1 + dist_to_com(map, node),
+        None => {
+            assert_eq!(node, "COM");
+            0
+        }
+    }
+}
+
+fn orbitals<'a>(map: &'a HashMap<&'a str, &'a str>, node: &'a str) -> HashSet<&'a str> {
+    match map.get(node) {
+        Some(node) => {
+                let mut rest = orbitals(map, node);
+                rest.insert(node);
+                rest
+        },
+        None => {
+            assert_eq!(node, "COM");
+            HashSet::new()
+        }
+    }
+}
+
+fn solve(input: &'static str, _part: usize) -> usize {
+    let map = parse(input);
+
+    map.keys().map(|node| dist_to_com(&map, node)).sum()
 }
 
 fn solve1(input: &'static str) -> usize { solve(input, 1) }
-fn solve2(input: &'static str) -> usize { solve(input, 2) }
+fn solve2(input: &'static str) -> usize {
+    let map = parse(input);
+
+    let santa = orbitals(&map, "SAN");
+    let me = orbitals(&map, "YOU");
+    let leg1 = me.sub(&santa);
+    let leg2 = santa.sub(&me);
+    leg1.len() + leg2.len()
+}
 
 //------------------------------ RUNNERS
 
 #[allow(unused)]
-// #[aoc(day6, part1)]
+#[aoc(day6, part1)]
 fn day6_part1(input: &'static str) -> usize {
     let ans = solve1(input);
-    // assert_eq!(ans, 0);
+    assert_eq!(ans, 247089);
     ans
 }
 
 #[allow(unused)]
-// #[aoc(day6, part2)]
+#[aoc(day6, part2)]
 fn day6_part2(input: &'static str) -> usize {
     let ans = solve2(input);
-    // assert_eq!(ans, 0);
+    assert_eq!(ans, 442);
     ans
 }
 
 //------------------------------ SAMPLE DATA
-const _SAMPLE: &str = "1234";
+const _SAMPLE: &str = "COM)B
+B)C
+C)D
+D)E
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L";
+
+const _SAMPLE2: &str = "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN";
