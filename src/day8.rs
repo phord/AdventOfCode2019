3450657bdc589ad3a0487a1017e73a962fc3b030
Phil Hord <phord@purestorage.com>
2022-12-10 19:33:28 -0800

    Day 8

--- src/day8.rs
+++ src/day8.rs
@@ -4,43 +4,63 @@ use yaah::aoc;
 use crate::*;
 
 //------------------------------ TESTS
-#[test] fn test_day8_part1() { assert_eq!(solve1(_SAMPLE), 1); }
-#[test] fn test_day8_part2() { assert_eq!(solve2(_SAMPLE), 2); }
+#[test] fn test_day8_part1() { assert_eq!(solve("123456789012", (3,2), 1), 1); }
 
-//------------------------------ PARSE INPUT
-
-fn parse(input: &'static str) -> Vec<&str> {
-    input.lines()
-        .collect()
-}
 
 //------------------------------ SOLVE
 
-fn solve(input: &'static str, part: usize) -> usize {
-    parse(input);
-    part
+fn solve(input: &'static str, size: (usize, usize), _part: usize) -> usize {
+    let (w, h) = size;
+    let mut min = h * w;
+    let mut ans = 0;
+    let mut image = vec![2; h*w];
+    for layer in input.as_bytes().chunks(h * w) {
+        if layer.len() == h*w {
+            // dbg!(std::str::from_utf8(layer).unwrap());
+
+            // Build image by stacking layers
+            for (i, b) in layer.iter().enumerate() {
+                if *b < b'2' && image[i] == 2 {
+                    image[i] = *b - b'0';
+                }
+            }
+
+            let zeros = layer.iter().filter(|&&x| x==b'0').count();
+            if zeros < min {
+                min = zeros;
+                let ones = layer.iter().filter(|&&x| x==b'1').count();
+                let twos = layer.iter().filter(|&&x| x==b'2').count();
+                ans = ones * twos;
+                // dbg!(zeros, ones, twos, min, ans);
+            }
+        }
+    }
+
+    for row in 0..h {
+        let xx = image[row*w..row*w+w].iter().map(|&x| if x == 0 {" "} else {"#"}).collect::<String>();
+        println!("{}", xx);
+    }
+    ans
 }
 
-fn solve1(input: &'static str) -> usize { solve(input, 1) }
-fn solve2(input: &'static str) -> usize { solve(input, 2) }
+fn solve1(input: &'static str) -> usize { solve(input, (25,6), 1) }
 
 //------------------------------ RUNNERS
 
 #[allow(unused)]
-// #[aoc(day8, part1)]
+#[aoc(day8, part1)]
 fn day8_part1(input: &'static str) -> usize {
     let ans = solve1(input);
     // assert_eq!(ans, 0);
     ans
 }
 
-#[allow(unused)]
-// #[aoc(day8, part2)]
-fn day8_part2(input: &'static str) -> usize {
-    let ans = solve2(input);
-    // assert_eq!(ans, 0);
-    ans
-}
 
 //------------------------------ SAMPLE DATA
-const _SAMPLE: &str = "1234";
+const _SAMPLE: &str = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const _CODE: &str = "#  #  ##  #  # ####  ##
+# #  #  # #  #    # #  #
+##   #  # #  #   #  #  #
+# #  #### #  #  #   ####
+# #  #  # #  # #    #  #
+#  # #  #  ##  #### #  # ";
